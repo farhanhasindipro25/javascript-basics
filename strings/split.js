364923bfb8ff68cee583807bd351fbdb13d9c21e
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2022-08-11 02:54:13 +0600

    fd

---
+++ strings/split.js
@@ -0,0 +1,10 @@
+const lyrics = "Now I am gone. You will be gone too! Keep Waiting.";
+// Split words by space
+const words = lyrics.split(' ');
+console.log(words);
+// Split sentences by fullstops
+const sentences = lyrics.split('.');
+console.log(sentences);
+// Split characters
+const characters = lyrics.split('');
+console.log(characters);
